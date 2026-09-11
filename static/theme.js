7f1a6503c0a2cf425c505c689538eee28dd9e903
ostlight <rhainich@astro.physik.uni-potsdam.de>
2026-09-11 12:37:44 +0200

    Fixes theme issue

--- static/theme.js
+++ static/theme.js
@@ -1,10 +1,16 @@
 (function () {
+  var theme = null;
   try {
-    var theme = localStorage.getItem("ost-theme");
-    if (theme === "dark" || theme === "light") {
-      document.documentElement.setAttribute("data-theme", theme);
-    }
+    theme = localStorage.getItem("ost-theme");
   } catch (e) {
     /* ignore */
   }
+  if (theme !== "dark" && theme !== "light") {
+    try {
+      theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    } catch (e) {
+      theme = "light";
+    }
+  }
+  document.documentElement.setAttribute("data-theme", theme);
 })();
